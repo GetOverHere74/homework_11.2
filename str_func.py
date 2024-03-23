ca27dcc0f21a07713dffd8c685d61199be58ab3f
skyl1 <skyl1.maxla2@mail.ru>
2024-03-23 17:36:51 +0500

    add new func and doc

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
 def upper_func(input_string):
+    """На вход поступают все заглавные буквы"""
     return input_string.upper()
+
+def upper_title(input_string):
+    """Ставит каждые первые буквы заглавными"""
+    return input_string.title()
